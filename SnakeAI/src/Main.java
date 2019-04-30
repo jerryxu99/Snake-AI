eeb65b453025ee9c7fb20de35526eea08f40e318
jerryxu99 <48665319+jerryxu99@users.noreply.github.com>
2019-04-29 21:22:50 -0700

    Update Main.java

--- SnakeAI/src/Main.java
+++ SnakeAI/src/Main.java
@@ -1,7 +1,7 @@
 /*
  * This is a snake game started on March 23, 2019. After finishing a human-controlled snake game, I wanted to
  * challenge my skills by programming an AI to play the game. Finally, after undergoing countless problems and bugs,
- * I have created a nearly-perfect snake AI. Completion date: April 23, 2019. This is my first personal project.
+ * I have created a nearly-perfect snake AI. Completion date: April 16, 2019. This is my first personal project.
  */
 public class Main {
     static Game game;
